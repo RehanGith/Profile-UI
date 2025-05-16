db6f45db87bc782cf903bbb36d1cf9f513f9e86f
RehanGith <chrehan1007@gmail.com>
2025-05-16 09:03:26 +0500

    made annotated string for description section

--- app/src/main/java/com/example/profileui/ProfileUI.kt
+++ app/src/main/java/com/example/profileui/ProfileUI.kt
@@ -26,6 +26,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -38,15 +41,18 @@ fun MyUi(modifier: Modifier = Modifier) {
         TopBar(name = "Rehan Pervaiz")
         Spacer(modifier = Modifier.height(5.dp))
         StatBar()
+        Description(profileName = "Muhammad Rehan", description = "This is a description\n" + "I'm a Software Engineer", url = "www.google.com", followedBy = listOf("Rehan","Rehan", "Aliya"), otherCount = 17)
 
     }
 }
 
 @Composable
-fun TopBar(name : String, modifier: Modifier = Modifier ) {
-    Row(modifier = modifier.fillMaxWidth(),
+fun TopBar(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically) {
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Icon(
             imageVector = Icons.Default.ArrowBack,
             contentDescription = "Back Arrow Icons",
@@ -73,30 +79,35 @@ fun TopBar(name : String, modifier: Modifier = Modifier ) {
         )
 
 
-
     }
 
 }
+
 @Composable
 fun StatBar(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxWidth(),
+    Row(
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        RoundIcon(image = painterResource(id = R.drawable.profile), modifier = Modifier
-            .size(100.dp)
-            .weight(3f))
+        RoundIcon(
+            image = painterResource(id = R.drawable.rehan), modifier = Modifier
+                .size(100.dp)
+                .weight(3f)
+        )
         StatSection(modifier = Modifier.weight(7f))
 
     }
 }
+
 @Composable
 fun RoundIcon(
-    image : Painter,
+    image: Painter,
     modifier: Modifier = Modifier
 ) {
     Icon(
         painter = image,
         contentDescription = null,
+        tint = Color.Unspecified,
         modifier = modifier
             .aspectRatio(1f, matchHeightConstraintsFirst = true)
             .clip(CircleShape)
@@ -109,6 +120,7 @@ fun RoundIcon(
 
     )
 }
+
 @Composable
 fun StatSection(modifier: Modifier = Modifier) {
     Row(
@@ -121,16 +133,99 @@ fun StatSection(modifier: Modifier = Modifier) {
         ShowStates(numberText = "2", text = "Following")
     }
 }
+
 @Composable
-fun ShowStates(numberText : String, text : String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier,
+fun ShowStates(numberText: String, text: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Text(text = numberText,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = numberText,
             fontWeight = FontWeight.Bold,
-            fontSize = 20.sp)
+            fontSize = 20.sp
+        )
         Spacer(modifier = Modifier.height(4.dp))
-        Text(text = text,
-            fontSize = 15.sp)
+        Text(
+            text = text,
+            fontSize = 15.sp
+        )
+    }
+}
+
+@Composable
+fun Description(
+    profileName: String,
+    description: String,
+    url: String,
+    followedBy : List<String>,
+    otherCount : Int,
+    modifier: Modifier = Modifier
+) {
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = profileName,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+        )
+        Text(
+            text = description,
+            style = TextStyle(
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+        )
+        Text(
+            text = url,
+            color = Color(0xFF3D3D91),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+        )
+        if(followedBy.isNotEmpty()) {
+            Text(
+                text = buildAnnotatedString {
+                    val boldStyle = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    append("Followed By: ")
+                    followedBy.forEachIndexed { index, s ->
+                        pushStyle(boldStyle)
+                        append(s)
+                        pop()
+                        if(index < followedBy.size - 1) {
+                            append(", ")
+                        }
+                    }
+                    if(otherCount > 2) {
+                        append(" and ")
+                        pushStyle(boldStyle)
+                        append("$otherCount others")
+                    }
+                },
+                style = TextStyle(
+                    letterSpacing = letterSpacing,
+                    lineHeight = lineHeight
+                )
+
+            )
+        }
+
+
     }
 }
